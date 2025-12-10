225938c5c4edefb70fb9c555368cbe0a5355007c
kwwk <kwwk747@gmail.com>
2025-12-10 13:48:40 +0100

    Create basic project structure

---
+++ CarbonIntensity_API_Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace CarbonIntensity_API_Tests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
