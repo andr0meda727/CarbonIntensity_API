3cf945c01e86d18e1a9f150ec9ffee64f1d5548a
kwwk <kwwk747@gmail.com>
2025-12-10 20:48:48 +0100

    Implement Energy Mix endpoint
    I added CarbonIntensityController with the "GET api/carbonintensity/energy-mix" endpoint.
    This endpoint delegates the request to ICarbonIntensity service.

--- CarbonIntensity_API/Controllers/CarbonIntensityController.cs
+++ CarbonIntensity_API/Controllers/CarbonIntensityController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CarbonIntensity_API.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CarbonIntensity_API.Controllers;
 
@@ -6,11 +7,29 @@ namespace CarbonIntensity_API.Controllers;
 [Route("api/[controller]")]
 public class CarbonIntensityController : ControllerBase
 {
-    // service
-    
-    
+    private readonly ICarbonIntensity _carbonIntensityService;
+
+    public CarbonIntensityController(ICarbonIntensity carbonIntensityService)
+    {
+        _carbonIntensityService = carbonIntensityService;
+    }
+
     // GetAverageEnergyMix method (today, tommorow, the day after tommorow)
+    [HttpGet("energy-mix")]
+    public async Task<IActionResult> GetAverageEnergyMix()
+    {
+        try
+        {
+            var result = await _carbonIntensityService.GetAverageEnergyMixAsync();
+            return Ok(result);
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, ex);
+        }
+    }
     
     
+    // [HttpGet("optimal-window")]
     // GetOptimalWindow method
 }
