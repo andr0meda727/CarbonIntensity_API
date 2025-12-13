4de718fd0cbbd88aaf2d4bd2ab97a32b01ab22d5
kwwk <kwwk747@gmail.com>
2025-12-13 17:44:34 +0100

    Configure services in Program.cs
    Added configuration for CarbonIntensity API base URL from appsettings.json
    Registered HttpClient for ICarbonIntensity service with base address validation
    Added controllers, Swagger, and endpoints API explorer
    Setup app to use Swagger in development environment

--- CarbonIntensity_API/Program.cs
+++ CarbonIntensity_API/Program.cs
@@ -2,10 +2,29 @@ using CarbonIntensity_API.Services;
 using CarbonIntensity_API.Services.Interfaces;
 
 var builder = WebApplication.CreateBuilder(args);
+var apiUrl = builder.Configuration["CarbonIntensityApi:BaseUrl"];
 
 builder.Services.AddControllers();
-builder.Services.AddHttpClient<ICarbonIntensity, CarbonIntensityService>();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddHttpClient<ICarbonIntensity, CarbonIntensityService>(client =>
+{
+    if (string.IsNullOrWhiteSpace(apiUrl)) 
+    {
+        throw new InvalidOperationException("CarbonIntensityApi:BaseUrl is missing in appsettings.json");
+    }
+    
+    client.BaseAddress = new Uri(apiUrl);
+});
 
 var app = builder.Build();
 
-app.Run();
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.MapControllers();
+
+await app.RunAsync();
