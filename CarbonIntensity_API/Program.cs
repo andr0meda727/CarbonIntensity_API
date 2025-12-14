8ddd7ecc3c08a3d1a047b58d1f3fdc16922fc5eb
kwwk <kwwk747@gmail.com>
2025-12-14 20:17:44 +0100

    Add CORS

--- CarbonIntensity_API/Program.cs
+++ CarbonIntensity_API/Program.cs
@@ -4,9 +4,24 @@ using CarbonIntensity_API.Services.Interfaces;
 var builder = WebApplication.CreateBuilder(args);
 var apiUrl = builder.Configuration["CarbonIntensityApi:BaseUrl"];
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowReactApp",
+        policy =>
+        {
+            policy.WithOrigins(
+                    "http://localhost:5173",
+                    "https://carbonintensity-frontend.onrender.com"
+                )
+                .AllowAnyHeader()
+                .AllowAnyMethod();
+        });
+});
+
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
 builder.Services.AddHttpClient<ICarbonIntensity, CarbonIntensityService>(client =>
 {
     if (string.IsNullOrWhiteSpace(apiUrl)) 
@@ -24,7 +39,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
+app.UseHttpsRedirection();
+app.UseCors("AllowReactApp");
 app.MapControllers();
 
 await app.RunAsync();
